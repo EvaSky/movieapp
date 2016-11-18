101300d0bb59c5b39f215f5eda82663ffd641e13
EvaSky <olga.shahray@gmail.com>
2016-11-18 22:12:39 +0200

    add logger

--- src/main/java/by.epam/movieapp/Main.java
+++ src/main/java/by.epam/movieapp/Main.java
@@ -4,7 +4,11 @@ package by.epam.movieapp;
  * @author Olga Shahray
  */
 public class Main {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(Main.class);
+
     public static void main(String[] args) {
+        LOG.debug("before");
         System.out.println("In main");
+        LOG.debug("after");
     }
 }
