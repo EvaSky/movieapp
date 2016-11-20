68cd063a66a17b39f819d0a210678b944b32e7f9
EvaSky <olga.shahray@gmail.com>
2016-11-20 23:15:01 +0200

    add spring test

--- src/test/java/by/epam/movieapp/web/mock/UserSpringMockTest.java
+++ src/test/java/by/epam/movieapp/web/mock/UserSpringMockTest.java
@@ -1,7 +1,35 @@
 package by.epam.movieapp.web.mock;
 
+import by.epam.movieapp.model.User;
+import by.epam.movieapp.web.rest.UserRestController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @author Olga Shahray
  */
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
 public class UserSpringMockTest {
+
+    @Autowired
+    private UserRestController controller;
+
+    @Test
+    public void testSave(){
+        controller.save(new User(0, "User"));
+    }
+
+    @Test
+    public void testDelete(){
+        controller.delete(1);
+    }
+
+    @Test
+    public void testGetAll(){
+        controller.getAll();
+    }
 }
